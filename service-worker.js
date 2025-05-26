5276f935f52059089b1e633b67f2a92ad38a8483
adandachi <93785785+adandachi@users.noreply.github.com>
2025-05-26 10:07:32 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,20 @@
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('workout-app-cache').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/WIRKOUTS-WIP.html',
-        '/styles.css', // Include any CSS files you need to cache
-        '/script.js', // Include your JavaScript files
-        '/icons/icon-192x192.png', // Include icons
-        '/icons/icon-512x512.png',
-      ]);
-    })
-  );
-});
+event.waitUntil(
+  caches.open('workout-app-cache').then((cache) => {
+    const repoName = '/workout-app'; // <<< Replace with your actual repository name!
+    return cache.addAll([
+      repoName + '/', // Caches the root of your project
+      repoName + '/001_Push.html', // Assuming this is your main page now
+      repoName + '/WIRKOUTS-WIP.html', // If this page exists
+      repoName + '/styles.css',
+      repoName + '/script.js',
+      repoName + '/icons/icon-192x192.png',
+      repoName + '/icons/icon-512x512.png',
+    ]);
+  })
+);
+}
+);
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
